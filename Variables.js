8c5296dee0525f9f361172350e907f2db2fad61b
Rommel Gonzalez <abril_17.92@hotmail.com>
2020-09-24 21:15:11 -0500

    Lesson 2. varibles String

--- Variables.js
+++ Variables.js
@@ -1,7 +1,23 @@
-var name = 'Rommel', lastName = 'Gonzalez' ;
+/* 
+ *Lesson 1. Variables
+*/
+var firstName = 'Rommel'; 
+var lastName = 'Gonzalez' ;
 var age = 28;
 
-console.log('Hello '+name+' '+lastName);
+console.log('Hello '+firstName+' '+lastName);
 console.log('Tengo '+age);
+console.log(typeof(firstName));
 
-console.log(typeof(name));
+/* 
+ *Lesson 2. Variables: Strings
+*/
+
+var nameUpperCase = firstName.toUpperCase();
+var lastNameLoweCase = lastName.toLowerCase();
+var nameLenght = lastName.length;
+var firstLetterName = firstName.charAt(0);
+var fullName = `${firstName} ${lastName.toUpperCase()}`;
+var str = firstName.substr(1,3);
+
+console.log(str);
