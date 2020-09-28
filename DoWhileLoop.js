73242fd380c51d75c801e965db2b02930a48e86f
Rommel Gonzalez <abril_17.92@hotmail.com>
2020-09-27 19:59:02 -0500

    Do-While Loop

---
+++ DoWhileLoop.js
@@ -0,0 +1,10 @@
+//Do While Loop
+var contador = 0;
+
+const llueve = () => Math.random() < 0.25;
+
+do {
+    contador++;
+} while (!llueve());
+// Reto 
+console.log(`Fui a ver si llovia ${contador} ${contador > 1 ? 'veces' : 'vez'}`);
